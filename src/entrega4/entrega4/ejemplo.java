b659f747a22221bfe1d51a2e32c648a5c9df1784
HP <lcala419@gmail.com>
2021-01-25 18:35:53 +0100

    por terminar

--- src/entrega4/entrega4/ejemplo.java
+++ src/entrega4/entrega4/ejemplo.java
@@ -1,14 +1,16 @@
 package entrega4.entrega4;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.List;
 import java.util.Scanner;
 
 public class ejemplo {
     public static void main (String[] args){
-try {
+
     Scanner teclado = new Scanner(System.in);
 
     String exePath = "C:\\Users\\HP\\Downloads\\geckodriver.exe";
@@ -17,19 +19,28 @@ try {
     WebDriver pagina1 = new FirefoxDriver();
     WebDriver pagina2 = new FirefoxDriver();
 
-    pagina1.get("http://www.fnac.es");
+    pagina1.get("https://www.pccomponentes.com");
     pagina2.get("http://www.phonehouse.es");
 
     System.out.println("¿Que desea buscar?");
     String Busqueda = teclado.nextLine();
 
-  /*  WebElement ventanaCookies =  pagina1.findElement(By.xpath("/html/body/aside/div/button"));
-    if (ventanaCookies !=null){
-        System.out.println("Detectado caja de cookies");
-    ventanaCookies.click();
-    }*/
+   try {
+       WebElement ventanaCookies = pagina1.findElement(By.id("onetrust-banner-sdk"));
+       if (ventanaCookies != null) {
+           System.out.println("Detectado caja de cookies");
+           ventanaCookies.click();
+       }
+       WebElement ventanasCookies = pagina2.findElement(By.xpath("/html/body/div[6]"));
+       if (ventanaCookies != null) {
+           System.out.println("Detectado caja de cookies");
+           ventanaCookies.click();
+       }
 
-    WebElement cajaBusqueda = pagina1.findElement(By.id("Fnac_Search"));
+   }catch (Exception e) {
+   }
+
+    WebElement cajaBusqueda = pagina1.findElement(By.xpath("/html/body/header/div[3]/div[1]/div/div[2]/div/form/input"));
     cajaBusqueda.sendKeys(Busqueda);
     cajaBusqueda.submit();
 
@@ -37,16 +48,14 @@ try {
     cajaBusqueda2.sendKeys(Busqueda);
     cajaBusqueda2.submit();
 
-    String resultados1 = pagina1.findElement(By.xpath("//*[@id=\"dontTouchThisDiv\"]")).getText();
+   String resultados1 = pagina1.findElement(By.xpath("//*[@id=\"pcc-search-api--hits\"]/div[2]/ol")).getText();
     System.out.println(resultados1);
 
-    String resultados2 = pagina2.findElement(By.xpath("//*[@id=\"flexboxgrid\"]")).getText();
-    System.out.println(resultados2);
+        System.out.println("--------------------------------");
 
+    String resultados= pagina2.findElement(By.xpath("//*[@id=\"productsList1\"]")).getText();
+    System.out.println(resultados);
 
-}catch (Exception e){
-    System.out.println("error");
-}
     }
 
 }
